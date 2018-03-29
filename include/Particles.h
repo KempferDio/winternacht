5f8d9ad5ea6ce79186d4a4d4f56e10792c0f79b6
Blair <kitan012@mail.ru>
2018-03-29 13:58:18 +0300

    Minor changes

---
+++ include/Particles.h
@@ -0,0 +1,14 @@
+#ifndef PARTICLES_H
+#define PARTICLES_H
+#include <Sprite.h>
+#include <SDL2/SDL.h>
+
+namespace Core {
+    class Particles {
+    public:
+        Sprite sprite;
+    private:
+    };
+}
+
+#endif
